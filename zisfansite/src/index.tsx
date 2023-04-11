a23298474abcf8f54d4194537a77d7aaf6d7be8e
ZiChengHuang915 <zichuang127@gmail.com>
2023-04-11 15:15:15 -0400

    added eslint with prettier

--- zisfansite/src/index.tsx
+++ zisfansite/src/index.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+console.log('Hello world!');
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
