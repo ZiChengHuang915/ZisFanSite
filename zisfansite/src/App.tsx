eb2618d512df2663571d00e66102951f6cea7554
ZiChengHuang915 <zichuang127@gmail.com>
2023-04-11 22:09:18 -0400

    Added title page without styling

--- zisfansite/src/App.tsx
+++ zisfansite/src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Stack from '@mui/material/Stack';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Typography variant="h3">Zi Cheng Huang</Typography>
+            <Typography variant="body1">
+                I'm a Computer Science student at the University of Waterloo. I'm a big fan of enthusiast desktop
+                hardware as well as PC building. In my spare time, I like to play badminton and Minecraft.
+            </Typography>
+            <Stack direction="row" spacing={2}>
+                <EmailIcon></EmailIcon>
+                <LinkedInIcon></LinkedInIcon>
+                <GitHubIcon></GitHubIcon>
+            </Stack>
         </div>
     );
 }
